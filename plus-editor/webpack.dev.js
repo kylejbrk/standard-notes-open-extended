c4583b43abe2ad4231ba2642359c76f5fe275eea
kylejbrk <kylejbrk@users.noreply.github.com>
2021-03-12 00:54:01 +0000

    deploy: c3ace0491308cfab4f71cffe33981493b9cbad7a

--- plus-editor/webpack.dev.js
+++ plus-editor/webpack.dev.js
@@ -10,7 +10,7 @@ module.exports = merge(config, {
   },
   devServer: {
     port: 8001,
-    contentBase: path.join(__dirname, 'dist'),
+    contentBase: path.resolve(__dirname, 'dist'),
     disableHostCheck: true,
     historyApiFallback: true,
     watchOptions: { aggregateTimeout: 300, poll: 1000 },
