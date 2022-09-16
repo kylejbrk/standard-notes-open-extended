48b7e84cd77a608a14ed6deb7864186b7a494625
kylejbrk <github@kyleburke.net>
2022-09-16 13:07:09 -0400

    new build script

--- build.py
+++ build.py
@@ -6,14 +6,15 @@ def unzip_all(zip_dir, output_dir):
     for f in os.listdir(zip_dir):
         if f.endswith('.zip'):
             zip_path = os.path.join(zip_dir, f)
-            output_path = os.path.join(output_dir, f.replace('.zip', ''))
+            # SN doesn't seem to like "." in the file path
+            output_path = os.path.join(output_dir, f.replace('.zip', '').replace('.', '_'))
 
             with zipfile.ZipFile(zip_path) as z:
                 z.extractall(output_path)
 
             print(f'Output contents of {zip_path} to {output_path}')
 
-            create_index(output_path)
+            # create_index(output_path)
 
 def create_index(output_path):
     package_file = 'package.json'
@@ -26,10 +27,20 @@ def create_index(output_path):
         index = {
             'name': package_info['name'],
             'version': package_info['version'],
-            # 'description': package_info['description'],
-            'url': 'http://127.0.0.1:5500/' + output_path + '/' + package_info.get('sn', {}).get('main', 'index.html')
+            'description': package_info.get('description', ''),
+            'url': 'http://127.0.0.1:5500/' + output_path.replace('\\', '/') + '/' + package_info.get('sn', {}).get('main', 'index.html'),
+            'latest_url': 'http://127.0.0.1:5500/' + output_path.replace('\\', '/') + '/' + 'index.json'
         }
-        
+
+        if 'theme' in output_path:
+            index['area'] = 'themes' 
+            index['content_type'] = 'SN|Theme'
+        else:
+            index['area'] = 'editor-editor'
+            index['content_type'] = 'SN|Component'        
+
+        print(index['latest_url'])
+
         with open(os.path.join(output_path, 'index.json'), 'w') as f:
             json.dump(index, f, indent = 4)
     else:
